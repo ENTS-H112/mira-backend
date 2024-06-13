9f7bc1d09e51b4c6a7a7e3681d4dd46861fbc613
Anggun Caksono <caksonoanggun@gmail.com>
2024-06-13 11:02:32 +0700

    Update Firebase private key handling

--- config/firebase.js
+++ config/firebase.js
@@ -2,11 +2,15 @@ const admin = require('firebase-admin');
 const dotenv = require('dotenv');
 
 dotenv.config();
+
+const base64PrivateKey = process.env.FIREBASE_PRIVATE_KEY;
+const privateKey = Buffer.from(base64PrivateKey, 'base64').toString('utf8');
+
 const serviceAccount = {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: privateKey,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID, 
   auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -15,10 +19,6 @@ const serviceAccount = {
   client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
   universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
 };
-
-if (!serviceAccount.private_key) {
-  throw new Error('FIREBASE_PRIVATE_KEY is not defined');
-}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
